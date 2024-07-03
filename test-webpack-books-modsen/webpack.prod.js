fed8db341110c006eb7ebfe84a9954832767bd03
DioMain <dima123.zample@gmail.com>
2024-07-03 23:27:45 +0300

    fix: vercel 2

--- test-webpack-books-modsen/webpack.prod.js
+++ test-webpack-books-modsen/webpack.prod.js
@@ -1,13 +1,9 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true
-  },
-  plugins: [
-    new Dotenv()
-  ]
+  }
 });
