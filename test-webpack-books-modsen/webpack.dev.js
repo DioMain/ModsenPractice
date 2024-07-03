5efb3b525dba4d16e9ed8c215bf156417aff5e1c
DioMain <dima123.zample@gmail.com>
2024-07-04 00:02:49 +0300

    test: vercel

--- test-webpack-books-modsen/webpack.dev.js
+++ test-webpack-books-modsen/webpack.dev.js
@@ -1,6 +1,7 @@
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   mode: "development",
@@ -12,5 +13,10 @@ module.exports = merge(common, {
     compress: true,
     port: 8080,
     allowedHosts: 'all',
-  }
+  },
+  plugins: [
+    new Dotenv({
+      path: "./.env.local"
+    })
+  ]
 });
